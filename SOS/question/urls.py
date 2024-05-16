60cf6e72cf91a3c985eddf6ca46b3549c32ab209
gom31 <gom12602@gmail.com>
2024-05-16 20:13:27 +0900

    add ursl

--- SOS/question/urls.py
+++ SOS/question/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('main/', views.main, name='main'),
     path('logout/', views.logout_view, name='logout'),
     path('signup/', views.signup, name='signup'),
+    path('quiz/', views.quiz, name='quiz'),
+    path('retest/', views.retest, name='retest'),
+    path('study/', views.study, name='study'),
 ]
