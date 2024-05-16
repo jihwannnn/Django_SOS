d314de5ec7b8205f8ccd41966b1e01acc9a138a4
Hwang Seongmin <hsmin040326@gmail.com>
2024-05-16 18:08:21 +0900

    models created. (questions, examlog, solvedquestions)

--- SOS/question/models.py
+++ SOS/question/models.py
@@ -1,3 +1,32 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
+User = get_user_model
+
 # Create your models here.
+
+class Questions(models.Model):
+    chapter = models.IntegerField()
+    image = models.ImageField(upload_to="quiz_images/")
+    answer = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.answer
+
+class examLog(models.Model):
+    username = models.ForeignKey (User,
+                                  on_delete=models.CASCADE,
+                                  related_name='username')
+    chapter = models.IntegerField()
+    examDateTime = models.DateTimeField()
+    examResult = models.JSONField()
+
+class SolvedQuestions(models.Model):
+    username = models.ForeignKey (User,
+                                  on_delete=models.CASCADE,
+                                  related_name='username')
+    solvedQuestions = models.ForeignKey(Questions,
+                                       on_delete=models.CASCADE,
+                                       related_name='solved questions')
+    wasRight = models.BooleanField()
+    submitted_answer = models.CharField()
