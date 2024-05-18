1fff1728f2c74b801e3d907327d9058e71efad6f
Hyunsoo Shin <shadowshin6@gmail.com>
2024-05-18 21:34:01 +0900

    fixed 3D image / modal window h2

--- SOS/static/script/main.js
+++ SOS/static/script/main.js
@@ -64,4 +64,26 @@ document.addEventListener('DOMContentLoaded', function() {
     studyChapter10Btn.onclick = function() {
         window.location.href = "study.html?chapter_num=10";
     }
+
+    // 이미지 컨테이너 마우스 움직임에 따른 효과
+    var container = document.querySelector('.image-container');
+    var overlay = document.querySelector('.overlay');
+
+    container.addEventListener('mousemove', function(e) {
+        var x = e.offsetX;
+        var y = e.offsetY;
+        var rotateY = -1 / 5 * x + 20;
+        var rotateX = 4 / 30 * y - 20;
+
+        overlay.style.backgroundPosition = `${x / 5 + y / 5}%`;
+        overlay.style.filter = `opacity(${x / 200}) brightness(1.2)`;
+
+        container.style.transform = `perspective(350px) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+    });
+
+    container.addEventListener('mouseout', function() {
+        overlay.style.filter = 'opacity(0)';
+        container.style.transform = 'perspective(350px) rotateY(0deg) rotateX(0deg)';
+    });
+    
 });
