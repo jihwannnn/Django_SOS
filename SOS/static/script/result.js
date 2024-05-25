e72dbda742607a4c068f83db267c3499afb80e21
gom31 <gom12602@gmail.com>
2024-05-25 20:47:47 +0900

    retest

--- SOS/static/script/result.js
+++ SOS/static/script/result.js
@@ -1,4 +1,4 @@
-console.log('quizscript.js loaded'); // 파일이 로드되었는지 확인
+console.log('result.js loaded'); // 파일이 로드되었는지 확인
 
 document.addEventListener('DOMContentLoaded', function () {
     console.log('DOMContentLoaded event fired'); // 디버깅용 로그
