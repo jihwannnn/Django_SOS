26ec2758c2112e3d20ff54214ff75d1da29eff67
gom31 <gom12602@gmail.com>
2024-05-25 22:10:39 +0900

    delete debug

--- SOS/static/script/result.js
+++
@@ -1,5 +0,0 @@
-console.log('quizscript.js loaded'); // 파일이 로드되었는지 확인
-
-document.addEventListener('DOMContentLoaded', function () {
-    console.log('DOMContentLoaded event fired'); // 디버깅용 로그
-})
--- SOS/static/script/script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('loginForm').addEventListener('submit', function(event) {
-    const username = document.getElementById('username').value.trim();
-    const password = document.getElementById('password').value.trim();
-
-    if (!username || !password) {
-        event.preventDefault();
-        alert('Please fill in both fields.');
-    }
-});
