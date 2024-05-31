ea77fa28d8a91cea5d93443c4fe899c449f0faac
Hwang Seongmin <hsmin040326@gmail.com>
2024-05-31 17:04:31 +0900

    created middleware.py to check login in every pages

--- SOS/SOS/settings.py
+++ SOS/SOS/settings.py
@@ -52,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'question.middleware.LoginRequiredMiddleware', # for login checking in every page
 ]
 
 ROOT_URLCONF = 'SOS.urls'
