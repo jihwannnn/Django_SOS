6ba8436bf09342dfa5b8668e5a64a6ca4a72f2a4
Hwang Seongmin <hsmin040326@gmail.com>
2024-06-02 19:27:34 +0900

    comments on models.py

--- SOS/question/models.py
+++ SOS/question/models.py
@@ -3,6 +3,8 @@ from django.db import models
 
 # Create your models here.
 
+# A model that stores the questions.
+# Each question has chapter number, image of the question, and its correct answer.
 class Question(models.Model):
     chapter = models.IntegerField()
     image = models.ImageField(upload_to="quiz_images/")
@@ -11,6 +13,9 @@ class Question(models.Model):
     def __str__(self):
         return self.answer
 
+# A model that stroes the questions that users solved.
+# Each record contains the user who solved, the question that was solved, 
+# boolean indicating whether the user's answer was correct, and the user's answer.
 class SolvedQuestion(models.Model):
     user = models.ForeignKey (User,
                               on_delete=models.CASCADE,
@@ -21,9 +26,13 @@ class SolvedQuestion(models.Model):
     was_right = models.BooleanField()
     submitted_answer = models.CharField(max_length=255)
 
+    # Ensure that each user can solve a particular quesiton only once(if the user solve again, update.)
     class Meta:
         unique_together = ('user', 'solved_questions')
 
+# A model that stores the record of exams in Quiz page and Retest page.
+# Each record contains the user who solved, the chapter it belongs to,
+# total number of questions that are provided, number of questions which the user got correctly.
 class ExamLog(models.Model):
     user = models.ForeignKey (User,
                               on_delete=models.CASCADE,
